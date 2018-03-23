0d964d40f0a90b61078eff48ec271643c3b9d672
Antoine Gagné <gagnantoine@gmail.com>
2018-03-23 14:52:10 -0400

    Add grammar types

--- src/Parser/Types.hs
+++ src/Parser/Types.hs
@@ -7,6 +7,40 @@ import Control.Lens
     )
 import qualified Data.Text as T
 
+data Output = Output
+    { _outOfBandRecord :: [OutOfBandRecord]
+    , _resultRecord :: Maybe ResultRecord
+    } deriving (Show)
+
+data ResultRecord = ResultRecord
+    { _token :: Maybe Token
+    , _resultClass :: ResultClass
+    , _results :: [Result]
+    } deriving (Show)
+
+data OutOfBandRecord
+    = OutOfBandAsyncRecord AsyncRecord
+    | OutOfBandStreamRecord StreamRecord
+    deriving (Show)
+
+data AsyncRecord
+    = ExecAsyncOutput (Maybe Token) AsyncOutput
+    | StatusAsyncOutput (Maybe Token) AsyncOutput
+    | NotifyAsyncOutput (Maybe Token) AsyncOutput
+    deriving (Show)
+
+data AsyncOutput = AsyncOutput
+    { _asyncClass :: AsyncClass
+    , _asyncResults :: [Result]
+    } deriving (Show)
+
+type Token = Integer
+
+data AsyncClass
+    = Stopped
+    | Others
+    deriving (Show)
+
 data Result = Result
     { _variable :: T.Text
     , _value :: Value
@@ -38,4 +72,7 @@ data StreamRecord
     | LogStreamOutput T.Text
     deriving (Show)
 
-$(makeLenses ''Result)
+makeLenses ''Result
+makeLenses ''AsyncOutput
+makeLenses ''ResultRecord
+makeLenses ''Output
