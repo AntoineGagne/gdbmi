ed5b857a6cc485cd0229ef2ae9a56bf828ae571b
Antoine Gagné <gagnantoine@gmail.com>
2018-03-25 17:22:55 -0400

    Add more tests

--- test/Parser/ParserSpec.hs
+++ test/Parser/ParserSpec.hs
@@ -25,6 +25,11 @@ spec = do
     listSpec
     resultSpec
     valueSpec
+    asyncClassSpec
+    asyncOutputSpec
+    asyncRecordSpec
+    resultRecordSpec
+    outputSpec
 
 tokenSpec :: Spec
 tokenSpec =
@@ -97,6 +102,129 @@ prop_idempotenceOfValueParsing value'
     = case parse Parser.value "" (showValue value') of
           Left _ -> False
           Right value'' -> value'' == value'
+
+asyncClassSpec :: Spec
+asyncClassSpec =
+    describe "asyncClass" $
+        context "given valid async class input" $
+            prop "returns the same async class as the input" prop_idempotenceOfAsyncClassParsing
+
+prop_idempotenceOfAsyncClassParsing :: AsyncClass -> Bool
+prop_idempotenceOfAsyncClassParsing asyncClass'
+    = case parse Parser.asyncClass "" (showAsyncClass asyncClass') of
+          Left _ -> False
+          Right asyncClass'' -> asyncClass'' == asyncClass'
+
+asyncOutputSpec :: Spec
+asyncOutputSpec =
+    describe "asyncOutput" $
+        context "given valid async output input" $
+            prop "returns the same async output as the input" prop_idempotenceOfAsyncOutputParsing
+
+prop_idempotenceOfAsyncOutputParsing :: AsyncOutput -> Bool
+prop_idempotenceOfAsyncOutputParsing asyncOutput'
+    = case parse Parser.asyncOutput "" (showAsyncOutput asyncOutput') of
+          Left _ -> False
+          Right asyncOutput'' -> asyncOutput'' == asyncOutput'
+
+asyncRecordSpec :: Spec
+asyncRecordSpec =
+    describe "asyncRecord" $
+        context "given valid async record input" $
+            prop "returns the same async record as the input" prop_idempotenceOfAsyncRecordParsing
+
+prop_idempotenceOfAsyncRecordParsing :: AsyncRecord -> Bool
+prop_idempotenceOfAsyncRecordParsing asyncRecord'
+    = case parse Parser.asyncRecord "" (showAsyncRecord asyncRecord') of
+          Left _ -> False
+          Right asyncRecord'' -> asyncRecord'' == asyncRecord'
+
+outOfBandRecordSpec :: Spec
+outOfBandRecordSpec =
+    describe "outOfBandRecord" $
+        context "given valid out of band record input" $
+            prop "returns the same out of band record as the input" prop_idempotenceOfOutOfBandRecordParsing
+
+prop_idempotenceOfOutOfBandRecordParsing :: OutOfBandRecord -> Bool
+prop_idempotenceOfOutOfBandRecordParsing outOfBandRecord'
+    = case parse Parser.outOfBandRecord "" (showOutOfBandRecord outOfBandRecord') of
+          Left _ -> False
+          Right outOfBandRecord'' -> outOfBandRecord'' == outOfBandRecord'
+
+resultRecordSpec :: Spec
+resultRecordSpec =
+    describe "resultRecord" $
+        context "given valid result record input" $
+            prop "returns the same result record as the input" prop_idempotenceOfResultRecordParsing
+
+prop_idempotenceOfResultRecordParsing :: ResultRecord -> Bool
+prop_idempotenceOfResultRecordParsing resultRecord'
+    = case parse Parser.resultRecord "" (showResultRecord resultRecord') of
+          Left _ -> False
+          Right resultRecord'' -> resultRecord'' == resultRecord'
+
+outputSpec :: Spec
+outputSpec =
+    describe "output" $
+        context "given valid output input" $
+            prop "returns the same output as the input" prop_idempotenceOfOutputParsing
+
+prop_idempotenceOfOutputParsing :: Output -> Bool
+prop_idempotenceOfOutputParsing output'
+    = case parse Parser.output "" (showOutput output') of
+          Left _ -> False
+          Right output'' -> output'' == output'
+
+instance Arbitrary Output where
+    arbitrary = do
+        outOfBandRecords <- arbitrary :: Gen [OutOfBandRecord]
+        resultRecord <- arbitrary :: Gen (Maybe ResultRecord)
+        pure $ Output outOfBandRecords resultRecord
+
+instance Arbitrary ResultRecord where
+    arbitrary = do
+        token <- arbitrary :: Gen (Maybe (Positive Integer))
+        resultClass <- arbitrary :: Gen ResultClass
+        results <- arbitrary :: Gen [Result]
+        pure $ ResultRecord (getPositive <$> token) resultClass results
+
+
+instance Arbitrary OutOfBandRecord where
+    arbitrary = do
+        n <- choose (0, 1) :: Gen Integer
+        case n of
+            0 -> OutOfBandAsyncRecord <$> (arbitrary :: Gen AsyncRecord)
+            1 -> OutOfBandStreamRecord <$> (arbitrary :: Gen StreamRecord)
+
+instance Arbitrary AsyncRecord where
+    arbitrary = do
+        n <- choose (0, 2) :: Gen Integer
+        token <- arbitrary :: Gen (Maybe (Positive Integer))
+        asyncOutput <- arbitrary :: Gen AsyncOutput
+        pure $ case n of
+            0 -> ExecAsyncOutput (getPositive <$> token) asyncOutput
+            1 -> StatusAsyncOutput (getPositive <$> token) asyncOutput
+            2 -> NotifyAsyncOutput (getPositive <$> token) asyncOutput
+
+instance Arbitrary AsyncOutput where
+    arbitrary = do
+        asyncClass <- arbitrary :: Gen AsyncClass
+        asyncResults <- arbitrary :: Gen [Result]
+        pure $ AsyncOutput asyncClass asyncResults
+
+instance Arbitrary AsyncClass where
+    arbitrary = do
+        n <- choose (0, 8) :: Gen Integer
+        pure $ case n of
+            0 -> Stopped
+            1 -> ThreadGroupAdded
+            2 -> ThreadGroupStarted
+            3 -> ThreadCreated
+            4 -> AsyncClassRunning
+            5 -> ThreadExited
+            6 -> ThreadGroupExited
+            7 -> BreakpointModified
+            8 -> LibraryLoaded
 
 instance Arbitrary List where
     arbitrary = sized $ \n -> frequency
